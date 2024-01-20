b2239ea71bf6777bfefef2041d8ad574a0bca306
pijushghorai <pghorai098@gmail.com>
2024-01-20 20:07:02 +0530

    first commit

---
+++ While and do while loop/doWhile.js
@@ -0,0 +1,22 @@
+// While loop
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index ${index}`);
+    index = index + 2
+}
+
+// While loop using array
+let myArray = ["Batman", "Superman", "Spiderman", "Ironman"]
+let i = 0
+while (i < myArray.length) {
+    console.log(myArray[i]);
+    i++
+}
+
+// do-while loop
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
