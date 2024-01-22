027b53b4e101ba3bbf14ef4a31f4a5b33feef943
pijushghorai <pghorai098@gmail.com>
2024-01-22 23:47:43 +0530

    first commit

---
+++ Filter map and reduce/01.js
@@ -0,0 +1,7 @@
+// const myFevCartoons = ["Doraemon", "Tom & Jerry", "Shinchan", "Ban 10", "Mr. Bean"]
+// const value = myFevCartoons.forEach((item)=>{
+//     console.log(item);
+//     return item
+// })
+// console.log(value);
+
