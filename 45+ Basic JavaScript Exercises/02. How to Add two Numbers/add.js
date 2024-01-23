6f18552ad32e8f183d3b67bf8e70177ee6485ed4
pijushghorai <pghorai098@gmail.com>
2024-01-23 16:19:24 +0530

    first commit

---
+++ 45+ Basic JavaScript Exercises/02. How to Add two Numbers/add.js
@@ -0,0 +1,4 @@
+const num1 = 15;
+const num2 = 12;
+const result = num1 + num2
+console.log(`Result ${num1} + ${num2} = ${result}`);
---
+++ 45+ Basic JavaScript Exercises/03. Add Two Numbers Entered by the User/addTwoNums.js
@@ -0,0 +1,6 @@
+const num1 = prompt("Enter first number")
+const num2 = prompt("Enter second number")
+const number1 = parseInt(num1)
+const number2 = parseInt(num2)
+const result = number1 + number2
+console.log(`Result ${number1} + ${number2} = ${result}`);
